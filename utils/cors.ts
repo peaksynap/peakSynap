61382a66d8f42ecd0e7327d53342a1b75dd665a7
Brandon cavichioni <brandon.cavichioni@etc-latam.com>
2024-11-20 18:18:51 -0600

    .

--- utils/cors.ts
+++ utils/cors.ts
@@ -1,19 +1,19 @@
 import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// Inicializa el middleware de CORS
 const cors = Cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  origin: '*', 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  origin: 'http://localhost:8081', 
 });
 
-export function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: (req: NextApiRequest, res: NextApiResponse, callback: (result?: any) => void) => void): Promise<void> {
+export function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: (req: NextApiRequest, res: NextApiResponse, callback: (result?: any) => void) => void): Promise<any> {
   return new Promise((resolve, reject) => {
     fn(req, res, (result: unknown) => {
       if (result instanceof Error) {
         return reject(result);
       }
-      return resolve();
+      return resolve(result);
     });
   });
 }
