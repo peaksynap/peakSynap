9533ce0516ae4effe4d01c938edafb35afb1d28b
Brandon cavichioni <brandon.cavichioni@etc-latam.com>
2025-04-06 14:41:02 -0600

    ajuste

--- pages/api/publications/index.ts
+++ pages/api/publications/index.ts
@@ -2,11 +2,29 @@ import { deleteOnePublication, listPublicPublications, newPublication, refreshPu
 import { authenticateToken } from '@/middleware/auth';
 import { IPublication } from '@/models';
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
 
 type Data = {error: string} |  IPublication 
 
-function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    
+
+const cors = Cors({
+    origin: '*', // PERMITE TODOS LOS ORÍGENES
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  })
+  
+  // Wrapper para usar middleware async
+  function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+        return resolve(result)
+      })
+    })
+  }
+async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    await runMiddleware(req,res,cors)
     switch (req.method) {
         case 'POST':
             return newPublication(req, res);
